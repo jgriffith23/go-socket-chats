bf3ff34a5b29863cc994ff47b0e18c79092e6847
Jennifer <jennifer.gd42@gmail.com>
2018-02-15 12:08:35 -0800

    Finish setting up globals and helpful types

--- app.go
+++ app.go
@@ -9,3 +9,23 @@ import (
     "net/http"
     "github.com/gorilla/websocket"
 )
+
+// FIXME: Rework this code so that the variables don't have to be global.
+
+// Connected clients.
+// Golang note: calling make() actually initializes the map in memory.
+var clients = make(map[*websocket.Conn]bool)
+
+// A queue for messages sent by clients.
+var broadcast = make(chan Message)
+
+// An object that turns a normal HTTP connection into a WebSocket.
+// FIXME: Write a CheckOrigin function for the Upgrader.
+var upgrader = websocket.Upgrader{}
+
+// An object to contain user messages.
+type Message struct {
+    Email string `json:"email"`
+    Username string `json:"username"`
+    Message string `json:"message"`
+}
